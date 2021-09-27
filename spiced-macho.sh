03ca2a2a2feddb8e5705add32fc908663e56dcd0
Sidmaz666 <sidmazumder8@gmal.com>
2021-09-28 00:24:25 +0530

    initial commit

---
+++ spiced-macho.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+reader="brave"
+manual=$(apropos -s ${SECTION:-''} ${@:-.} | \
+    grep -v -E '^.+ \(0\)' |\
+    awk '{print $2 "    " $1}' | \
+    sort -R | \
+    rofi -dmenu -i -p "Man-Pages " -theme gruvbox-dark | \
+    sed -E 's/^\((.+)\)/\1/')
+open (){
+man -Tpdf $manual > /home/$USER/Documents/Manuals/$utility_name.pdf  
+$reader /home/$USER/Documents/Manuals/$utility_name.pdf
+}
+if [ ! -z "$manual" ]; then
+utility_name="$(echo "$manual" | sed 's/[[:space:]]//g' | sed 's/./&-/1' )"
+if [[ ! -d "/home/$USER/Documents/Manuals" ]]; then
+mkdir /home/$USER/Documents/Manuals
+open && exit
+else
+open && exit
+fi
+else
+exit
+fi
+
