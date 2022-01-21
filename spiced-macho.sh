b3b4bc90bfd37700e6616effe209c0dac443b7ae
Sid <sidmazumder8@gmail.com>
2022-01-21 15:32:21 +0530

    Update spiced-macho.sh

--- spiced-macho.sh
+++ spiced-macho.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-reader="brave"
+reader="brave --app=file://"
 manual=$(apropos -s ${SECTION:-''} ${@:-.} | \
     grep -v -E '^.+ \(0\)' |\
     awk '{print $2 "    " $1}' | \
@@ -8,7 +8,7 @@ manual=$(apropos -s ${SECTION:-''} ${@:-.} | \
     sed -E 's/^\((.+)\)/\1/')
 open (){
 man -Tpdf $manual > /home/$USER/Documents/Manuals/$utility_name.pdf  
-$reader /home/$USER/Documents/Manuals/$utility_name.pdf
+$reader/home/$USER/Documents/Manuals/$utility_name.pdf
 }
 if [ ! -z "$manual" ]; then
 utility_name="$(echo "$manual" | sed 's/[[:space:]]//g' | sed 's/./&-/1' )"
